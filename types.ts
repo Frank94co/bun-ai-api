79e958b35d3638d532f3c3e20efc796218c937a2
Frank Sebastián Franco Hernández <frank9412_co@hotmail.com>
2025-12-29 21:57:14 -0500

    Implementación de groq y cerebras

---
+++ types.ts
@@ -0,0 +1,8 @@
+export interface ChatMessage {
+    role: 'user' | 'assistant' | 'system';
+    content: string;
+}
+export interface AIService{
+    name: string;
+    chat: (messages: ChatMessage[])=>Promise<AsyncIterable<string>>;
+}
